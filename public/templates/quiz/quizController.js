524feccac34e0c0ed0a417b1cce5c880cc203b18
opiomorphz <39005902+opiomorphz@users.noreply.github.com>
2018-06-01 21:59:02 +0700

    1

--- public/templates/quiz/quizController.js
+++ public/templates/quiz/quizController.js
@@ -38,8 +38,7 @@ angular.module("flashlightForFutureApp")
 
     quiz.setAnswer = function(number) {
         var n = quiz.questions[quiz.questionNumber-1].points[number];
-        quiz.discCounters[number - 1]++;
-
+        quiz.discCounters[number]++;
         quiz.personalityCounters[n-1]++;
 
         var changeHighest = quiz.personalityCounters[n-1] >=
@@ -66,16 +65,64 @@ angular.module("flashlightForFutureApp")
         quiz.questionNumber++;
         console.log(quiz.questionNumber);
         if(quiz.questionNumber > quiz.questions.length){
-            $http.get('/assets/data/quizResults.json').success(function(data){
+            $http.get('/assets/data/discResults.json').success(function(data){
                 quiz.results = data;
                 console.log("Successfully loaded " + quiz.results.length + " results");
             });
         }
     };
 
+    quiz.getDiscResult = function() {
+        
+        var discResult = '';
+        if(quiz.questionNumber >= 20){
+            var D = quiz.discCounters[0];
+            var I = quiz.discCounters[1];
+            var S = quiz.discCounters[2];
+            var C = quiz.discCounters[3];
+
+            if(D > I && D > S && D > C){
+                discResult = 'D';
+            }
+            else if(I > D && I > S && I > C){
+                discResult = 'I';
+            }
+            else if(S > D && S > I && S > C){
+                discResult = 'S';
+            }
+            else if(C > D && C > I && C > S){
+                discResult = 'C';
+            }
+            else{
+                var i = quiz.discCounters.indexOf(Math.max(quiz.discCounters));
+
+                if(D == S)
+                {
+                    discResult = 'D';
+                }
+                else if(D == I)
+                {
+                    discResult = 'I';
+                }
+                else if(I == S)
+                {
+                    discResult = 'S';
+                }
+                else if(S == C)
+                {
+                    discResult = 'C';
+                }
+            }
+            
+        }   
+        return discResult;
+    };
+
     quiz.showInstruction = function() {
         $('#instructionModal').modal('show');
     }
+
+
 
     setTimeout(quiz.showInstruction, 1000);
 });
